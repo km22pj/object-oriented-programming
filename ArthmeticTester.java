543eacb8b381089590202c3755efebe9ba44273c
pjh <105645876+pjhswpj@users.noreply.github.com>
2022-09-30 00:34:14 +0900

    Add files via upload

---
+++ ArthmeticTester.java
@@ -0,0 +1,14 @@
+
+public class ArthmeticTester {
+
+	public static void main(String[] args) {
+		int age1 = 18;
+		int age2 = 35;
+		int age3 = 50;
+		int age4 = 44;
+		double averageAge = (double) (age1+age2+age3+age4) / 4;
+		System.out.println(averageAge);
+
+	}
+
+}
